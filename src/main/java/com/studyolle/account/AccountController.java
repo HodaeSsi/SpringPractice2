946148308214bdc9c2e99b8de55ea0dfd600db92
HodaeSsi <kfdream123@gmail.com>
2022-02-07 22:37:47 +0900

    회원 가입 뷰 개발 및 테스트

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -9,6 +9,8 @@ public class AccountController {
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
+//        model.addAttribute("signUpForm", new SignUpForm());
+        model.addAttribute(new SignUpForm()); //attributeName 생략가능 : 캐멀케이스를 준수한 클래스명을 사용할 때
         return "account/sign-up";
     }
 }
