463a6d7f851d31ca0f655217580b3fc2b1d974c4
HodaeSsi <kfdream123@gmail.com>
2022-02-16 17:36:29 +0900

    회원 가입 폼 서브밋 처리

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,6 +1,9 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -18,6 +21,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     @InitBinder("signUpForm") //케멀케이스로 된 타입명에 반응함(SignUpForm)
     public void initBinder(WebDataBinder webDataBinder) {
@@ -37,6 +42,23 @@ public class AccountController {
         if(errors.hasErrors()) {
             return "account/sign-up";
         }
+        
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO password encoding 필요
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올래, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+            "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
 
         // TODO 회원 가입 처리
         return "redirect:/";
